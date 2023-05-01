41c941a0c0cc9732ed7c68202040391276ab1268
Nisha Pokharel <nishapokharel2018@gmail.com>
2023-05-01 10:39:22 +0545

    Deployement of ML model to identify class of iris flower

---
+++ ML model Deployement with Flask/model.py
@@ -0,0 +1,25 @@
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+import pickle
+
+df = pd.read_csv('IRIS.csv')
+
+print(df.head())
+
+x = df[['sepal_length','sepal_width','petal_length','petal_width']]
+y = df['species']
+
+x_train, x_test, y_train, y_test = train_test_split(x,y,test_size=0.3,random_state=50)
+
+sc = StandardScaler()
+
+x_train = sc.fit_transform(x_train)
+x_test = sc.transform(x_test)
+
+classifier = RandomForestClassifier()
+
+classifier.fit(x_train, y_train)
+
+pickle.dump(classifier, open("model.pkl", "ab"))
